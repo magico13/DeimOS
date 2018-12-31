64e9e883120796e92fe6c6453124ba50032ee3e9
Michael Marvin <magico1313@gmail.com>
2018-12-31 00:13:26 -0500

    Fully support the Keyboard app and the WiFi app now works on the Pi

--- App_WiFi.py
+++ App_WiFi.py
@@ -1,24 +1,38 @@
 from app import App
 import utils
 from utils import COLORS
+from App_Keyboard import App_Keyboard
 
 import pygame
 from wifi import Cell, Scheme
+from subprocess import call
+import traceback
 
 class App_WiFi(App):
     networks = []
-    dirty = False
     Run = True
+    Keyboard = None
     def __init__(self):
         super(App_WiFi, self).__init__()
         self.Buttons, self.Texts = utils.load(self, 'App_WiFi.json')
+        self.dirty = False
+        self.ssid = ''
+        self.passkey = ''
+        self.encrypted = False
 
     def FirstDraw(self, screen):
         #Draw the background
         self.DrawBackground(screen)
+        self.GetButtonByID('btnConnect').SetVisible(self.validate())
+            
         super(App_WiFi, self).FirstDraw(screen)
 
     def Draw(self, screen):
+        if self.Keyboard != None:
+            if not self.Keyboard.Initialized:
+                self.Keyboard.FirstDraw(screen)
+                return True
+            return self.Keyboard.Draw(screen)
         worked = False
         if self.dirty: #redraw
             self.FirstDraw(screen)
@@ -28,6 +42,17 @@ class App_WiFi(App):
         return worked
 
     def EventLoop(self, events):
+        if self.Keyboard != None:
+            self.Keyboard.EventLoop(events)
+            if self.Keyboard.Complete:
+                if not self.Keyboard.Cancelled: 
+                    self.passkey = self.Keyboard.Text
+                    self.GetTxtByID('txtPass').SetText('*'*len(self.passkey))
+                    print('Passkey: {}'.format(self.passkey))
+                self.Keyboard = None
+                self.dirty = True
+            return
+            
         for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 self.MouseClick(pygame.mouse.get_pos())
@@ -48,7 +73,7 @@ class App_WiFi(App):
         numNetworks = len(self.networks)
         for i in range(numNetworks):
             net = self.networks[i]
-            button = utils.Button('btnNetwork{}'.format(i), (50, 100+i*50), (250, 40), COLORS.BLUE, '{}'.format(net.ssid), COLORS.WHITE, self.Connect, 30)
+            button = utils.Button('btnNetwork{}'.format(i), (50, 100+i*50), (250, 40), COLORS.BLUE, '{}{}'.format('{E}' if net.encrypted else '', net.ssid), COLORS.WHITE, self.Select, 30)
             self.Buttons.append(button)
             print('{} - {} - {} - {}'.format(i, net.ssid, net.quality, net.encrypted))
         self.dirty = True
@@ -57,14 +82,56 @@ class App_WiFi(App):
         self.PopUps.append(popup)
             
 
-    def Connect(self, btnID):
-        print('Connecting')
+    def Select(self, btnID):
         i = int(btnID[10:])
         print(i)
         net = self.networks[i]
-        print('Network: {}'.format(net.ssid))
+        print('Selected Network: {}'.format(net.ssid))
+        self.ssid = net.ssid
+        self.passkey = ''
+        if net.encrypted:
+            #draw the keyboard
+            self.Keyboard = App_Keyboard('Enter passcode for {}:'.format(net.ssid))
+        self.GetTxtByID('txtSSID').SetText(self.ssid)
+        self.GetTxtByID('txtPass').SetText('None')
+        self.encrypted = net.encrypted
+        
+    def Connect(self, btnID):
+        print(btnID)
+        if not self.validate(): return
         #this is where we just insert it into wpa_supplicant ourselves
         #then call 'wpa_cli -i wlan0 reconfigure'
+        errored = False
+        try:
+            with open('/etc/wpa_supplicant/wpa_supplicant.conf', 'w') as wpa:
+                wpa.write('ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n')
+                wpa.write('update_config=1\n')
+                wpa.write('country=US\n\n')
+                wpa.write('network={\n')
+                wpa.write('    ssid="{}"\n'.format(self.ssid))
+                if self.encrypted:
+                    wpa.write('    psk="{}"\n'.format(self.passkey))
+                    wpa.write('    key_mgmt=WPA-PSK\n')
+                else:
+                    wpa.write('    key_mgmt=NONE\n')
+                wpa.write('}\n')
+            call(['wpa_cli', '-i', 'wlan0', 'reconfigure'])
+        except:
+            errored = True
+            print('Exception thrown when trying to configure network')
+            traceback.print_exc()
+            
+        self.ssid = ''
+        self.passkey = ''
+        self.encrypted = False
+        self.GetTxtByID('txtSSID').SetText('None' if not errored else 'ERROR')
+        self.GetTxtByID('txtPass').SetText('None')
+        self.dirty = True
+        
+            
+
+    def validate(self):
+        return self.ssid != '' and (not self.encrypted or self.passkey != '')    
 
 if __name__=="__main__":
     #os.environ["SDL_FBDEV"] = "/dev/fb1"
